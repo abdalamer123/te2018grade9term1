20898f1156476093df8ce1ee46ece9f52453b22d
catherine <cgenao80@gmail.com>
2018-10-20 15:39:26 -0400

    Update object.js
    i tried but on teusday can i get some extra help

---
+++ object.js
@@ -0,0 +1,23 @@
+function character(name){
+
+	this.name = name;
+	this.weapon = quirk
+	this.color;
+	this.realationship = false
+	this.class = "3A"
+}
+
+
+
+
+
+
+
+
+
+
+let midorya = new character ("midorya")
+midorya.color = "white"
+midorya.relationship = "pending"
+midorya.weapon = "one for all"
+midorya.class = "3A"
