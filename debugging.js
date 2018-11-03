b1df004c00fbedce1ced8f2aea544e719c50847e
catherine <cgenao80@gmail.com>
2018-11-03 15:50:29 -0400

    NANI
    NAIN

--- debugging.js
+++ debugging.js
@@ -28,13 +28,13 @@ console.log(money)
 let num1 = READLINE.question("Enter num1: ");
 let num2 = READLINE.question("Enter num2: ");
 let num3 = READLINE.question("Enter num3: ");
-if(num1 > num2) {
+if(num2 > num1) {
   console.log(`${num1} is the largest value.`);
 }
-else if(num2 > num3) {
+ if(num2 > num3) {
   console.log(`${num2} is the largest value.`);
 }
-else if ( num3 > num1){
+ if ( num3 > num2){
   console.log(`${num3} is the largest value.`);
 }
 
